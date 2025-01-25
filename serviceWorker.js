d5105692a211c11a31f7cb8af91465ebc3cae7c2
Noe97663 <73392168+Noe97663@users.noreply.github.com>
2025-01-25 16:24:24 +0530

    extension makes alert

---
+++ serviceWorker.js
@@ -0,0 +1,13 @@
+//can use the chrome api
+
+// adding function as a listener here, arg - tab
+chrome.action.onClicked.addListener (tab =>{
+    //script inline instead of separate file
+    chrome.scripting.executeScript({
+        target: {tabId:tab.id},
+        func: () => {
+            alert("Extension says hello")
+        }
+    });
+
+});
